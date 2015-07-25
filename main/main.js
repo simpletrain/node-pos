43ff6970aae7401e1541328440abf37177330260
simpletrain <zsy498761554@live.com>
2015-07-25 15:27:05 +0800

    add input

--- main/main.js
+++ main/main.js
@@ -1,3 +1,25 @@
-/**
- * Created by ming on 7/25/15.
- */
+function printReceipt(tags) {
+    var scanner = new Scanner('barcode');
+    var myCart = new Cart();
+    var myPos = new Pos(scanner, myCart);
+
+    myPos.scan(tags);
+    PromotionProcessor.CalculatePromotion(myCart);
+
+    console.log(myPos.printReceipt());
+
+}
+
+var tags = [
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000003-2',
+    'ITEM000005',
+    'ITEM000005',
+    'ITEM000005'
+];
+
+printReceipt(tags);
