f98af34529591edf7d5531895e2822934aca6c02
simpletrain <zsy498761554@live.com>
2015-07-27 20:35:09 +0800

    optimize cart method

--- main/model/cart.js
+++ main/model/cart.js
@@ -8,25 +8,24 @@ function Cart() {
 
 
 Cart.prototype.addCartItem = function (cartItem) {
-    var indexOfCart = this.findInCart(cartItem);
+    var _cartItem = this.findCartItem(cartItem);
 
-    if (indexOfCart !== -1) {
-        this.cartItems[indexOfCart].count += cartItem.count;
+    if (_cartItem) {
+        _cartItem.count += cartItem.count;
     } else {
         this.cartItems.push(cartItem);
     }
 };
 
 
-Cart.prototype.findInCart = function (cartItem) {
+Cart.prototype.findCartItem = function (cartItem) {
     var barcode = cartItem.item.barcode;
 
     for (var i = 0; i < this.cartItems.length; i++) {
         if (barcode === this.cartItems[i].item.barcode) {
-            return i;
+            return this.cartItems[i];
         }
     }
-    return -1;
 };
 
 module.exports = Cart;
