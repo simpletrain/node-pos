3622dce80f7d59a113fabdd9a4bc2e61912dcf17
simpletrain <zsy498761554@live.com>
2015-08-04 23:08:09 +0800

    finish item-spec

--- spec/item-spec.js
+++ spec/item-spec.js
@@ -1,3 +1,15 @@
-/**
- * Created by ming on 8/4/15.
- */
+var Item = require('../main/model/item.js');
+
+describe('Item', function () {
+
+
+    describe('.find()', function () {
+
+        it('should find item', function () {
+            var item = new Item('ITEM000004', '电池', '个', 2.00);
+            expect(Item.find('ITEM000004')).toEqual(item);
+        });
+
+    });
+
+});
